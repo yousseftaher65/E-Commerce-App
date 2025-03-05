6ba365d46a4da8a74d2872ba0d3678be4c5fae1e
yousseftaher65 <yousseftaher65@gmail.com>
2025-03-05 09:10:45 +0200

    add needed assets and translations

---
+++ lib/core/utils/assets.gen.dart
@@ -0,0 +1,438 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/add_1.png
+  AssetGenImage get add1 => const AssetGenImage('assets/icons/add_1.png');
+
+  /// File path: assets/icons/add_2.png
+  AssetGenImage get add2 => const AssetGenImage('assets/icons/add_2.png');
+
+  /// File path: assets/icons/arrow_back_1.png
+  AssetGenImage get arrowBack1 =>
+      const AssetGenImage('assets/icons/arrow_back_1.png');
+
+  /// File path: assets/icons/arrow_back_2.png
+  AssetGenImage get arrowBack2 =>
+      const AssetGenImage('assets/icons/arrow_back_2.png');
+
+  /// File path: assets/icons/cart_1.png
+  AssetGenImage get cart1 => const AssetGenImage('assets/icons/cart_1.png');
+
+  /// File path: assets/icons/cart_2.png
+  AssetGenImage get cart2 => const AssetGenImage('assets/icons/cart_2.png');
+
+  /// File path: assets/icons/category_1.png
+  AssetGenImage get category1 =>
+      const AssetGenImage('assets/icons/category_1.png');
+
+  /// File path: assets/icons/category_2.png
+  AssetGenImage get category2 =>
+      const AssetGenImage('assets/icons/category_2.png');
+
+  /// File path: assets/icons/delete.png
+  AssetGenImage get delete => const AssetGenImage('assets/icons/delete.png');
+
+  /// File path: assets/icons/error.png
+  AssetGenImage get error => const AssetGenImage('assets/icons/error.png');
+
+  /// File path: assets/icons/filter_1.png
+  AssetGenImage get filter1 => const AssetGenImage('assets/icons/filter_1.png');
+
+  /// File path: assets/icons/filter_2.png
+  AssetGenImage get filter2 => const AssetGenImage('assets/icons/filter_2.png');
+
+  /// File path: assets/icons/google.png
+  AssetGenImage get google => const AssetGenImage('assets/icons/google.png');
+
+  /// File path: assets/icons/home_1.png
+  AssetGenImage get home1 => const AssetGenImage('assets/icons/home_1.png');
+
+  /// File path: assets/icons/home_2.png
+  AssetGenImage get home2 => const AssetGenImage('assets/icons/home_2.png');
+
+  /// File path: assets/icons/info.png
+  AssetGenImage get info => const AssetGenImage('assets/icons/info.png');
+
+  /// File path: assets/icons/lock.png
+  AssetGenImage get lock => const AssetGenImage('assets/icons/lock.png');
+
+  /// File path: assets/icons/logout.png
+  AssetGenImage get logout => const AssetGenImage('assets/icons/logout.png');
+
+  /// File path: assets/icons/minus_1.png
+  AssetGenImage get minus1 => const AssetGenImage('assets/icons/minus_1.png');
+
+  /// File path: assets/icons/minus_2.png
+  AssetGenImage get minus2 => const AssetGenImage('assets/icons/minus_2.png');
+
+  /// File path: assets/icons/order.png
+  AssetGenImage get order => const AssetGenImage('assets/icons/order.png');
+
+  /// File path: assets/icons/payment_1.png
+  AssetGenImage get payment1 =>
+      const AssetGenImage('assets/icons/payment_1.png');
+
+  /// File path: assets/icons/payment_2.png
+  AssetGenImage get payment2 =>
+      const AssetGenImage('assets/icons/payment_2.png');
+
+  /// File path: assets/icons/payment_3.png
+  AssetGenImage get payment3 =>
+      const AssetGenImage('assets/icons/payment_3.png');
+
+  /// File path: assets/icons/privacy.png
+  AssetGenImage get privacy => const AssetGenImage('assets/icons/privacy.png');
+
+  /// File path: assets/icons/profile_1.png
+  AssetGenImage get profile1 =>
+      const AssetGenImage('assets/icons/profile_1.png');
+
+  /// File path: assets/icons/profile_2.png
+  AssetGenImage get profile2 =>
+      const AssetGenImage('assets/icons/profile_2.png');
+
+  /// File path: assets/icons/question.png
+  AssetGenImage get question =>
+      const AssetGenImage('assets/icons/question.png');
+
+  /// File path: assets/icons/rate_1.png
+  AssetGenImage get rate1 => const AssetGenImage('assets/icons/rate_1.png');
+
+  /// File path: assets/icons/rate_2.png
+  AssetGenImage get rate2 => const AssetGenImage('assets/icons/rate_2.png');
+
+  /// File path: assets/icons/review_1.png
+  AssetGenImage get review1 => const AssetGenImage('assets/icons/review_1.png');
+
+  /// File path: assets/icons/review_2.png
+  AssetGenImage get review2 => const AssetGenImage('assets/icons/review_2.png');
+
+  /// File path: assets/icons/review_3.png
+  AssetGenImage get review3 => const AssetGenImage('assets/icons/review_3.png');
+
+  /// File path: assets/icons/search_1.png
+  AssetGenImage get search1 => const AssetGenImage('assets/icons/search_1.png');
+
+  /// File path: assets/icons/search_2.png
+  AssetGenImage get search2 => const AssetGenImage('assets/icons/search_2.png');
+
+  /// File path: assets/icons/select_1.png
+  AssetGenImage get select1 => const AssetGenImage('assets/icons/select_1.png');
+
+  /// File path: assets/icons/select_2.png
+  AssetGenImage get select2 => const AssetGenImage('assets/icons/select_2.png');
+
+  /// File path: assets/icons/shipping_1.png
+  AssetGenImage get shipping1 =>
+      const AssetGenImage('assets/icons/shipping_1.png');
+
+  /// File path: assets/icons/shipping_2.png
+  AssetGenImage get shipping2 =>
+      const AssetGenImage('assets/icons/shipping_2.png');
+
+  /// File path: assets/icons/success.png
+  AssetGenImage get success => const AssetGenImage('assets/icons/success.png');
+
+  /// File path: assets/icons/terms.png
+  AssetGenImage get terms => const AssetGenImage('assets/icons/terms.png');
+
+  /// File path: assets/icons/theme.png
+  AssetGenImage get theme => const AssetGenImage('assets/icons/theme.png');
+
+  /// File path: assets/icons/warning.png
+  AssetGenImage get warning => const AssetGenImage('assets/icons/warning.png');
+
+  /// File path: assets/icons/wishlist_1.png
+  AssetGenImage get wishlist1 =>
+      const AssetGenImage('assets/icons/wishlist_1.png');
+
+  /// File path: assets/icons/wishlist_2.png
+  AssetGenImage get wishlist2 =>
+      const AssetGenImage('assets/icons/wishlist_2.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        add1,
+        add2,
+        arrowBack1,
+        arrowBack2,
+        cart1,
+        cart2,
+        category1,
+        category2,
+        delete,
+        error,
+        filter1,
+        filter2,
+        google,
+        home1,
+        home2,
+        info,
+        lock,
+        logout,
+        minus1,
+        minus2,
+        order,
+        payment1,
+        payment2,
+        payment3,
+        privacy,
+        profile1,
+        profile2,
+        question,
+        rate1,
+        rate2,
+        review1,
+        review2,
+        review3,
+        search1,
+        search2,
+        select1,
+        select2,
+        shipping1,
+        shipping2,
+        success,
+        terms,
+        theme,
+        warning,
+        wishlist1,
+        wishlist2
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/banner.png
+  AssetGenImage get banner => const AssetGenImage('assets/images/banner.png');
+
+  /// File path: assets/images/empty_cart.png
+  AssetGenImage get emptyCart =>
+      const AssetGenImage('assets/images/empty_cart.png');
+
+  /// File path: assets/images/empty_wishlist.png
+  AssetGenImage get emptyWishlist =>
+      const AssetGenImage('assets/images/empty_wishlist.png');
+
+  /// File path: assets/images/onboarding_1.png
+  AssetGenImage get onboarding1 =>
+      const AssetGenImage('assets/images/onboarding_1.png');
+
+  /// File path: assets/images/onboarding_2.png
+  AssetGenImage get onboarding2 =>
+      const AssetGenImage('assets/images/onboarding_2.png');
+
+  /// File path: assets/images/onboarding_3.png
+  AssetGenImage get onboarding3 =>
+      const AssetGenImage('assets/images/onboarding_3.png');
+
+  /// File path: assets/images/order_placed.png
+  AssetGenImage get orderPlaced =>
+      const AssetGenImage('assets/images/order_placed.png');
+
+  /// File path: assets/images/orders.png
+  AssetGenImage get orders => const AssetGenImage('assets/images/orders.png');
+
+  /// File path: assets/images/reset_password.png
+  AssetGenImage get resetPassword =>
+      const AssetGenImage('assets/images/reset_password.png');
+
+  /// File path: assets/images/watch.png
+  AssetGenImage get watch => const AssetGenImage('assets/images/watch.png');
+
+  /// File path: assets/images/watch_2.png
+  AssetGenImage get watch2 => const AssetGenImage('assets/images/watch_2.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        banner,
+        emptyCart,
+        emptyWishlist,
+        onboarding1,
+        onboarding2,
+        onboarding3,
+        orderPlaced,
+        orders,
+        resetPassword,
+        watch,
+        watch2
+      ];
+}
+
+class $AssetsLogosGen {
+  const $AssetsLogosGen();
+
+  /// File path: assets/logos/app_logo_dark.png
+  AssetGenImage get appLogoDark =>
+      const AssetGenImage('assets/logos/app_logo_dark.png');
+
+  /// File path: assets/logos/app_logo_light.png
+  AssetGenImage get appLogoLight =>
+      const AssetGenImage('assets/logos/app_logo_light.png');
+
+  /// File path: assets/logos/decor.png
+  AssetGenImage get decor => const AssetGenImage('assets/logos/decor.png');
+
+  /// File path: assets/logos/electronics.png
+  AssetGenImage get electronics =>
+      const AssetGenImage('assets/logos/electronics.png');
+
+  /// File path: assets/logos/equipment.png
+  AssetGenImage get equipment =>
+      const AssetGenImage('assets/logos/equipment.png');
+
+  /// File path: assets/logos/fashion.png
+  AssetGenImage get fashion => const AssetGenImage('assets/logos/fashion.png');
+
+  /// File path: assets/logos/furniture.png
+  AssetGenImage get furniture =>
+      const AssetGenImage('assets/logos/furniture.png');
+
+  /// File path: assets/logos/health.png
+  AssetGenImage get health => const AssetGenImage('assets/logos/health.png');
+
+  /// File path: assets/logos/industrial.png
+  AssetGenImage get industrial =>
+      const AssetGenImage('assets/logos/industrial.png');
+
+  /// File path: assets/logos/real_state.png
+  AssetGenImage get realState =>
+      const AssetGenImage('assets/logos/real_state.png');
+
+  /// File path: assets/logos/services.png
+  AssetGenImage get services =>
+      const AssetGenImage('assets/logos/services.png');
+
+  /// File path: assets/logos/tv.png
+  AssetGenImage get tv => const AssetGenImage('assets/logos/tv.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        appLogoDark,
+        appLogoLight,
+        decor,
+        electronics,
+        equipment,
+        fashion,
+        furniture,
+        health,
+        industrial,
+        realState,
+        services,
+        tv
+      ];
+}
+
+class $AssetsTranslationsGen {
+  const $AssetsTranslationsGen();
+
+  /// File path: assets/translations/ar.json
+  String get ar => 'assets/translations/ar.json';
+
+  /// File path: assets/translations/en.json
+  String get en => 'assets/translations/en.json';
+
+  /// List of all assets
+  List<String> get values => [ar, en];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLogosGen logos = $AssetsLogosGen();
+  static const $AssetsTranslationsGen translations = $AssetsTranslationsGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
