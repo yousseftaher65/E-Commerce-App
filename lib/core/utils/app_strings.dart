e9bb753121c1a6086ed6f69ed4b058d2945d3131
yousseftaher65 <yousseftaher65@gmail.com>
2025-04-07 13:07:36 +0200

    add skeleton package , missing assets and new color

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -178,4 +178,17 @@ class AppStrings {
   static String createAccount = 'create_account'.tr();
   static String send = 'send'.tr();
   static String forgotPasswordAppBar = 'forgot_password_app_bar'.tr();
+  static String readLess = 'read_less'.tr();
+  static String topRated = 'top_rated'.tr();
+  static String freeShipping = 'free_shipping'.tr();
+  static String productInfo = 'Constructed with high-quality silicone material, the Loop Silicone Strong Magnetic Watch ensures a comfortable and secure fit on your wrist';
+  static String productDescription = 'Constructed with high-quality silicone material, the Loop Silicone Strong Magnetic Watch ensures a comfortable and secure fit on your wrist. The soft and flexible silicone is gentle on the skin, making it ideal for extended wear. Its lightweight design allows for a seamless blend of comfort and durability.One of the standout features of this watch band is its strong magnetic closure. The powerful magnets embedded within the band provide a secure and reliable connection, ensuring that your smartwatch stays firmly in place throughout the day. Say goodbye to worries about accidental detachment or slippage - the magnetic closure offers a peace of mind for active individuals on the go.The Loop Silicone Strong Magnetic Watch Band is highly versatile, compatible with a wide range of smartwatch models. Its adjustable strap length allows for a customizable fit, catering to various wrist sizes. Whether you\'re engaging in intense workouts or attending formal occasions this watch band effortlessly adapts to your style and activity level.';
+  static String bestSeller = 'best_seller'.tr();
+  static String newArrival = 'new_arrival'.tr();
+  static String hotDeals = 'hot_deals'.tr();
+  static String spicialOffer = 'special_offer'.tr();
+  static String specialDiscount = 'special_discount'.tr();
+  static String specialPrice = 'special_price'.tr();
+  static String specialOffers = 'special_offers'.tr();
+
 }
